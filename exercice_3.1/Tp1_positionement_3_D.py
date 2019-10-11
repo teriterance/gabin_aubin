0c0d0014bd40d0d2dfc15c52fdd7b6b9d6276759
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-10-11 16:17:50 +0200

    COmmiting

---
+++ exercice_3.1/Tp1_positionement_3_D.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+from mpl_toolkits.mplot3d import Axes3D
+
+def grad_3_1(X, Y):
+    return Y, X
+
+def print_grad_2D(grad_f, start_x = -1, end_x = 1, start_y = -1, end_y = 1, nbx = 10, nby = 10):
+    """ This function is made to print the gradien of given function f """
+    X, Y = np.meshgrid(np.linspace(start_x, end_x, nbx), np.linspace(start_y, end_y, nby))
+    Gx, Gy = grad_f(X, Y)
+    plt.quiver(X, Y, Gx, Gy)
+    plt.show()
+
+def print_contour_line( start_x = -1, end_x = 1, start_y = -1, end_y = 1, nbx = 10, nby = 10):
+    X, Y = np.meshgrid(np.linspace(start_x, end_x, nbx), np.linspace(start_y, end_y, nby))
+    Z = X*Y
+    fig  = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, Z)
+    plt.show()
+
+def exercice_3_1():
+    #Soit f(x,y) = x*y on veut determiner 1
+    #le gradient de f au point x0, y0
+    #on a grad(f) =  y*x_^ + x *
+    # afichage du champ de vecteur 
+    print_contour_line( -5, 5, -5, 5, 100, 100)
+    
+
+if __name__ == "__main__":
+    exercice_3_1()
---
+++ lib_fonction.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sc
+
+
+def print_grad_2D(grad_f, start_x = -1, end_x = 1, start_y = -1, end_y = 1, nbx = 10, nby = 10):
+    """ This function is made to print the gradien of given function f """
+    X, Y = np.meshgrid(np.linspace(start_x, end_x, nbx), np.linspace(start_y, end_y, nby))
+    Gx, Gy = grad_f(X, Y)
+    plt.quiver(X, Y, Gx, Gy)
+    plt.show()
+
+def print_contour_line(f, start_x = -1, end_x = 1, start_y = -1, end_y = 1, nbx = 10, nby = 10):
+    X, Y = np.meshgrid(np.linspace(start_x, end_x, nbx), np.linspace(start_y, end_y, nby))
+    Z = X*Y
+    plt.contour(X, Y, Z)
+    plt.plot()
